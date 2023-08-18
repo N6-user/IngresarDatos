83c48597b2d31615bd14ea0788ba30cc5708ce23
Diyelmo <rodriguez_guillermo2004@hotmail.com>
2023-08-17 22:24:28 -0300

    Agregar funcionalidad al boton

--- src/ingresardatos/NewJFrame.java
+++ src/ingresardatos/NewJFrame.java
@@ -5,6 +5,8 @@
  */
 package ingresardatos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author nahue
@@ -31,7 +33,7 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        jtMail = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jPasswordField1 = new javax.swing.JPasswordField();
 
@@ -47,12 +49,17 @@ public class NewJFrame extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
         jLabel3.setText("Ingresar contraseña");
 
-        jTextField1.setFont(new java.awt.Font("Tahoma", 2, 13)); // NOI18N
-        jTextField1.setText("Correo");
-        jTextField1.setToolTipText("");
+        jtMail.setFont(new java.awt.Font("Tahoma", 2, 13)); // NOI18N
+        jtMail.setText("Correo");
+        jtMail.setToolTipText("");
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         jButton1.setText("Ingresar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jPasswordField1.setText("Contraseña");
 
@@ -71,7 +78,7 @@ public class NewJFrame extends javax.swing.JFrame {
                             .addComponent(jButton1)
                             .addComponent(jLabel3)
                             .addComponent(jLabel2)
-                            .addComponent(jTextField1)
+                            .addComponent(jtMail)
                             .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE))))
                 .addContainerGap(202, Short.MAX_VALUE))
         );
@@ -83,7 +90,7 @@ public class NewJFrame extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jtMail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(40, 40, 40)
                 .addComponent(jLabel3)
                 .addGap(18, 18, 18)
@@ -106,6 +113,15 @@ public class NewJFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(jtMail.getText().equals("alumno@ulp.com.ar")&& jPasswordField1.getText().equals("12345678")){
+            JOptionPane.showMessageDialog(this, "Usted ingreso correctamente a la pagina.");
+        }else{JOptionPane.showMessageDialog(this,"Error al ingresar los datos");}
+            
+            
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -149,6 +165,6 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jtMail;
     // End of variables declaration//GEN-END:variables
 }
